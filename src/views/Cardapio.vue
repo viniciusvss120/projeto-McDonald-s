91c2bc5a6abdb5a4938738a34acc7152c154b36d
Vinicius Silva Souza <viniciusvss120@gmail.com>
2023-01-07 09:51:41 -0400

    Editando o sidnav para ficar responsivo

--- src/views/Cardapio.vue
+++ src/views/Cardapio.vue
@@ -5,7 +5,7 @@
     </header>
 
     <section>
-      <nav class="container">
+      <nav class="container" v-show="false">
         <div class="cardapio">
           <aside class="nav">
             <router-link to="/cardapio" class="list"> <img src="../img/cardapio/sidnav/bovino.png" alt="">Nossos produtos</router-link>
@@ -54,6 +54,56 @@
       </nav>
     </section>
 
+    <section>
+      <nav class="container">
+        <div class="cardapio">
+          <!-- navigatio -->
+          <div class="itens-container">
+            <div class="navigation">
+              <div class="navigation-itens">
+                <ul>
+                  <li>
+                    <router-link to="/">inicio</router-link>
+                    <img src="https://www.mcdonalds.com.br/images/icons/actions-navigation-chevron-right-16.svg" alt="">
+                  </li>
+                  <li>
+                    <router-link to="/cardapio" v-if="showCardapio">Cardapio</router-link>
+                    <span v-else>Cardapio</span>
+                  </li>
+                  <li v-show="showNavigation" @click="alterNavigation()">
+                    <img src="https://www.mcdonalds.com.br/images/icons/actions-navigation-chevron-right-16.svg" alt="">
+                     <!-- <router-link to="/cardapio/Lançamento" v-if="showLancamento">Lançamento</router-link> -->
+                    <span>{{items}}</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="itens">
+              <router-view />
+            </div>
+          </div>
+
+          <aside class="nav">
+            <router-link to="/cardapio" class="list"> <img src="../img/cardapio/sidnav/bovino.png" alt="">Nossos produtos</router-link>
+            <div class="nav-itens">
+              <ul>
+                <li
+                  v-for="index in cardapio"
+                  :key="index.nome"
+                  @click="alterShowCardapio(index.nome)"
+                  >
+                  <router-link :to="index.routerImg" class="sidnav">
+                    <img :src="require(`../img/cardapio/sidnav/${index.img}`)" alt="">
+                     <span>{{index.nome}}</span>
+                  </router-link>
+                </li>
+              </ul>
+            </div>
+          </aside>
+        </div>
+      </nav>
+    </section>
+
     <footer>
       <Footer />
     </footer>
@@ -63,12 +113,14 @@
 <script>
 import Menu from '../components/Menu.vue'
 import Footer from '../components/Footer.vue'
+// import CardapioMobile from '@/components/CardapioMobile.vue'
 // import Produtos from '../components/Produtos.vue'
 export default {
   name: 'theCardapio',
   components: {
     Menu,
     Footer
+    // CardapioMobile
     // Produtos
   },
   data () {
@@ -284,5 +336,59 @@ export default {
   .navigation-itens a{
     text-decoration: underline;
     color: blue;
+  }
+
+  /* responsivo */
+  @media only screen and (max-width: 1000px) and (min-width: 350px) {
+    .container{
+      /* margin: 0 auto; */
+    }
+    .cardapio{
+      flex-wrap: wrap;
+      margin: 0px;
+    }
+
+    .nav{
+      width: 98%;
+      margin: 0px 0px 10px 5px;
+    }
+
+    .nav-itens ul{
+      display: flex;
+      flex-wrap: wrap;
+      gap: 15px;
+    }
+    .nav-itens ul li{
+      width: 40%;
+      margin: 0 auto;
+    }
+    .nav-itens ul li a {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      text-align: center;
+      padding: 15px;
+      color: #292929;
+    }
+    .nav-itens ul li span{
+      text-align: center;
+      font-weight: 500;
+      font-size: 00.885rem;
+    }
+
+    .nav a{
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      /* grid-template-columns: 1fr; */
+      /* justify-items: center; */
+      /* border: 1px solid #000; */
+    }
+
+    .sidnav:focus-within{
+    font-weight: bold;
+    border: 5px solid #ffb800;
+  }
   }
 </style>
